e1aad403a451bd7e3cf7bc6bad21e297f7501bb0
saitharun11 <62788879+saitharun11@users.noreply.github.com>
2020-03-30 14:04:53 +0530

    Update tharun_os.cpp

--- tharun_os.cpp
+++ tharun_os.cpp
@@ -1,64 +1,94 @@
 #include<bits/stdc++.h>
 using namespace std;
+class project{
+	public:
+	    int at=0,wt=0,tat=0,bt=0;
+	    float pri=0.0;
+	    bool done;
+};
 int main()
 {
-	int n;
+	int n,f=0;
 	cout<<"Enter no. of processes"<<endl;
 	cin>>n;
-	int wt[n]={0},bt[n]={0},at[n]={0},tat[n]={0};
-	float pri[n]={0.0};
-	bool done[n]={0};
+	project s[n];
 	int ma,mi,tot_sum,avg_wt,avg_tat,m=INT_MIN,pos=0,time;
 	ma=mi=INT_MAX;
 	tot_sum=avg_wt=avg_tat=0;
-	cout<<"Enter Arr.Time and Burst Time"<<endl;
+	cout<<"\nEnter Arr.Time and Burst Time"<<endl;
 	for(int i=0;i<n;i++){
-		cout<<"P"<<i+1<<endl;
-		cin>>at[i]>>bt[i];
-		tot_sum+=bt[i];
+		cout<<"\nP"<<i+1<<endl;
+		cin>>s[i].at>>s[i].bt;
+		tot_sum+=s[i].bt;
 	}
 	cout<<endl;
-	for(int i=0;i<n;i++) ma=min(ma,at[i]);
+	for(int i=0;i<n;i++) ma=min(ma,s[i].at);
 	for(int i=0;i<n;i++){
-		if(at[i]==ma && bt[i]<mi){
-			mi=bt[i];
+		if(s[i].at==ma && s[i].bt<mi){
+			mi=s[i].bt;
 			pos=i;
 		}
 	}
-	//cout<<"pos->"<<pos<<endl;
-	time=at[pos];
-	while(time<tot_sum){
-	    time=time+bt[pos];
-	    tat[pos]=time-at[pos];
-	    done[pos]=1;
-	    m=0;
+	time=s[pos].at;
+	time=time+s[pos].bt;
+	s[pos].tat=time-s[pos].at;
+	s[pos].done=1;
+	while(1){
+		f=0;
+		for(int i=0;i<n;i++){
+			if(!s[i].done){
+				f=1;
+				break;
+			}
+		}
+		if(!f) break;
+	    f=m=0;
 	    for(int i=0;i<n;i++){
-		    if(!done[i] && at[i]<time){
-			    wt[i]=time-at[i];
-		    pri[i]=1+((float)wt[i]/bt[i]);
-		    if(pri[i]>m){
-			    m=pri[i];
+		    if(!s[i].done && s[i].at<=time){
+		    	f=1;
+			    s[i].wt=time-s[i].at;
+		    s[i].pri=1+((float)s[i].wt/s[i].bt);
+		    if(s[i].pri>m){
+			    m=s[i].pri;
 			    pos=i;
 		    }
 		    }
 	    }
-	    //for(int i=0;i<n;i++) cout<<tat[i]<<" "<<wt[i]<<" "<<pri[i]<<endl;
-	//cout<<"pos->"<<pos<<endl;
+	    if(f){
+	    	time=time+s[pos].bt;
+	        s[pos].tat=time-s[pos].at;
+	        s[pos].done=1;
+		}
+		else
+		time++;
 	}
 	for(int i=0;i<n;i++) {
-	    avg_wt+=wt[i];
-		avg_tat+=tat[i];	
+	    avg_wt+=s[i].wt;
+		avg_tat+=s[i].tat;	
 	}
 	cout<<"Pid\tArr.T\tBrst.T\tWait.T\tTurnArnd.T\n"<<endl;
-	for(int i=0;i<n;i++) cout<<"P"<<i+1<<"\t"<<at[i]<<"\t"<<bt[i]<<"\t"<<wt[i]<<"\t"<<tat[i]<<endl<<endl;
+	for(int i=0;i<n;i++) cout<<"P"<<i+1<<"\t"<<s[i].at<<"\t"<<s[i].bt<<"\t"<<s[i].wt<<"\t"<<s[i].tat<<endl<<endl;
 	cout<<"Average Waiting time -> "<<(float)avg_wt/n<<endl;
 	cout<<"Average Turnaround time -> "<<(float)avg_tat/n<<endl;
 }
 /*Sample Test case
-5
-2 6
-5 2
-1 8
-0 3
-4 4
+
+4
+0 6
+5 1
+13 8
+17 1
+
+4
+0 8
+5 4
+13 9
+17 5
+
+4
+0 8
+5 4
+13 9
+17 5
+
 */
